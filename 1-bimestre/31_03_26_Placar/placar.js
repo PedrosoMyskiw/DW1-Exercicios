3aeeff2358ea2ee6b68d3fea1490c9d9774c0d19
PedrosoMyskiw <pedroso.myskiw@gmail.com>
2026-03-31 11:25:14 -0300

    DW1

---
+++ 1-bimestre/31_03_26_Placar/placar.js
@@ -0,0 +1,40 @@
+let pontosA = 0;
+let pontosB = 0;
+
+function marcarPonto(time) {
+    if (time === 'A') {
+        pontosA++;
+        document.getElementById("pontosA").innerText = pontosA;
+    } else {
+        pontosB++;
+        document.getElementById("pontosB").innerText = pontosB;
+    }
+
+    verificarVencedor();
+}
+
+function resetar() {
+    pontosA = 0;
+    pontosB = 0;
+
+    document.getElementById("pontosA").innerText = 0;
+    document.getElementById("pontosB").innerText = 0;
+
+    document.querySelectorAll(".card").forEach(card => {
+        card.classList.remove("vencedor");
+    });
+}
+
+function verificarVencedor() {
+    let cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => card.classList.remove("vencedor"));
+
+    if (pontosA >= 10) {
+        cards[0].classList.add("vencedor");
+    }
+
+    if (pontosB >= 10) {
+        cards[1].classList.add("vencedor");
+    }
+}
